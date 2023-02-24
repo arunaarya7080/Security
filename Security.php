695e3212bcefc2ff52dce2348de00a0135239bb9
Arun Kumar <79196733+arunaarya7080@users.noreply.github.com>
2023-02-24 11:27:24 +0530

    Update and rename Security to Security.php

---
+++ Security.php
@@ -0,0 +1,19 @@
+
+# Restricted pdf file 
+
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTP_COOKIE} !.*wordpress_logged_in.*$ [NC]
+    RewriteCond %{REQUEST_URI} ^(.*?/?)wp-content/uploads/.* [NC]
+    RewriteRule . http://%{HTTP_HOST}%1/404/?redirect_to=%{REQUEST_URI} [L,QSA]
+</IfModule>
+
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTP_COOKIE} !.*wordpress_logged_in.*$ [NC]
+    RewriteCond %{REQUEST_URI} ^(.*?/?)wp-content/uploads/.*\.(?:gif|png|jpe?g|pdf|txt|rtf|html|htm|xlsx?|docx?|mp3|mp4|mov)$ [NC]
+    RewriteRule . http://%{HTTP_HOST}%1/wp-login.php?redirect_to=%{REQUEST_URI} [L,QSA]
+</IfModule>
+
+
+# //Restricted pdf file 
